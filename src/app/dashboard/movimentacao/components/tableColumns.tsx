dd6ad4021accb97cfb1186061c4e16f0046108e1
pedroalvesss <pedropetros1@gmail.com>
2025-06-16 18:20:03 -0300

    feat: add movimentacoesTableColumns function to define table columns for Movimentacao

---
+++ src/app/dashboard/movimentacao/components/tableColumns.tsx
@@ -0,0 +1,78 @@
+import { Movimentacao } from "@/types";
+import type { ColumnDef } from "@tanstack/react-table";
+
+export function movimentacoesTableColumns(): ColumnDef<
+  Movimentacao | undefined
+>[] {
+  return [
+    {
+      accessorKey: "id",
+      header: () => {
+        return <div className="flex justify-center text-center">ID</div>;
+      },
+      cell: (cell) => (
+        <div className="flex justify-center text-center">
+          {cell.row.original?.id}
+        </div>
+      ),
+    },
+    {
+      accessorKey: "produtoid",
+      header: () => {
+        return (
+          <div className="flex justify-center text-center">ID do Produto</div>
+        );
+      },
+      cell: (cell) => (
+        <div className="flex justify-center text-center">
+          {cell.row.original?.produtoid}
+        </div>
+      ),
+    },
+    {
+      accessorKey: "produtonome",
+      header: () => {
+        return (
+          <div className="flex justify-center text-center">Nome do Produto</div>
+        );
+      },
+      cell: (cell) => (
+        <div className="flex justify-center text-center">
+          {cell.row.original?.produtonome}
+        </div>
+      ),
+    },
+    {
+      accessorKey: "quantidade",
+      header: () => {
+        return (
+          <div className="flex justify-center text-center">Quantidade</div>
+        );
+      },
+      cell: (cell) => {
+        return (
+          <div className="flex justify-center text-center">
+            {cell.row.original?.quantidade}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "tipo",
+      header: () => {
+        return (
+          <div className="flex justify-center text-center">
+            Tipo de Movimentação
+          </div>
+        );
+      },
+      cell: (cell) => {
+        return (
+          <div className="flex justify-center text-center">
+            {cell.row.original?.tipo}
+          </div>
+        );
+      },
+    },
+  ];
+}
